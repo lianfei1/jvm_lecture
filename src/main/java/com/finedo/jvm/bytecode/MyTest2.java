6fd0b21889b4d4cf37ee873b6dd07b5835792bdd
lianfei <597657148@qq.com>
2019-03-09 19:51:15 +0800

    add test

---
+++ src/main/java/com/finedo/jvm/bytecode/MyTest2.java
@@ -0,0 +1,32 @@
+package com.finedo.jvm.bytecode;
+
+public class MyTest2 {
+    String str = "welcome";
+    private int x = 5;
+    public static Integer in = 10 ;
+
+    public static void main(String[] args) {
+        MyTest2 myTest2 = new MyTest2();
+        myTest2.setX(8);
+        in = 20;
+    }
+
+    private synchronized void setX(int x) {
+        this.x = x;
+    }
+
+    private void test(String str) {
+        synchronized (str) {
+            System.out.println("hello world");
+        }
+    }
+
+
+    private synchronized static void test2() {
+
+    }
+
+    static {
+        System.out.println("welcome!");
+    }
+}
